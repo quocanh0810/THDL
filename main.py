1f9528bbc9113a24c05f817cc497a04987138f03
pdd-vn <dungphamdang99@gmail.com>
2024-07-17 08:32:33 +0700

    Enable all spiders

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
-from web_crawler.spiders import HacomSpider, PhongVuSpider, PhucanhSpider
+from web_crawler.spiders import HacomSpider, PhongVuSpider, PhucanhSpider  # noqa: F401
 
 settings = get_project_settings()
 process = CrawlerProcess(settings)
-# process.crawl(HacomSpider)
+process.crawl(HacomSpider)
 process.crawl(PhongVuSpider)
-# process.crawl(PhucanhSpider)
+process.crawl(PhucanhSpider)
 process.start()
